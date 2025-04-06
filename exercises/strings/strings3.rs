136644ec03d8d68b60affd2577d72024eb1a3617
TBYBY <1051733980@qq.com>
2025-04-06 22:24:15 +0800

    update

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -7,19 +7,20 @@
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    input.trim().to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let mut x = input.to_string();
+    x.push_str(" world!");
+    x
 }
-
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let mut x = input.replace("cars","balloons");
+    x
 }
-
 #[cfg(test)]
 mod tests {
     use super::*;
