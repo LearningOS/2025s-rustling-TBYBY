437e4069f3d423f3d806fb1bd12cb74c5ff475ac
TBYBY <1051733980@qq.com>
2025-04-06 11:12:42 +0800

    update

--- exercises/variables/variables4.rs
+++ exercises/variables/variables4.rs
@@ -5,8 +5,8 @@
 
 
 fn main() {
-    let mut x : i32 = 3;
-    println!("Number {x}" );
+    let mut x = 3;
+    println!("Number {}", x );
     x = 5; // don't change this line
-    println!("Number {x}");
+    println!("Number {}", x);
 }
